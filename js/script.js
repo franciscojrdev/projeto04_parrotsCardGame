d51476d081eb6d185efbf59b3c79da59c2307bc8
franciscojrdev <franciscojunior7222@gmail.com>
2022-06-20 10:31:35 -0300

    adicionando funções

--- js/script.js
+++ js/script.js
@@ -1,32 +1,50 @@
 
+let cartasJogo = [];
+let back, front;
+
 const cartas = ["bobrossparrot.gif", "explodyparrot.gif", "fiestaparrot.gif", "metalparrot.gif", "revertitparrot.gif", "tripletsparrot.gif", "unicornparrot.gif"]
 
-let quantCartas = parseInt(prompt("Informe a quantidade de cartas"));
 
-while (quantCartas % 2 !== 0){
-    quantCartas = parseInt(prompt("Informe a quantidade de cartas"));
+function quantCards(){}
+let quantCartas = parseInt(prompt("Informe a quantidade de cartas [Entre 4 e 14 cartas]"));
+
+while (quantCartas % 2 !== 0 || quantCartas < 4 || quantCartas > 14) {
+    quantCartas = parseInt(prompt("Informe a quantidade de cartas [Entre 4 e 14 cartas]"));
 }
 function criarCartas() {
+    cartasJogo = cartas.slice(0, quantCartas / 2);
+    for (let i = 0; cartasJogo.length < quantCartas; i++) {
+        cartasJogo.push(cartas[i]);
+    }
+    cartasJogo.sort(function comparador() {
+        return Math.random() - 0.5;
+    });
+
     for (let i = 0; i < quantCartas; i++) {
         const template = ` 
-            <div class="card" onclick="turnCard(this)">
+            <li class="card" onclick="turnCard(this)">
+                <div class="back-face face" >
+                    <img src="./img/${cartasJogo[i]}" alt="Card Front">
+                </div>
                 <div class="front-face face">
-                    <img src="img/${cartas[i]}" alt="Card Front">
+                    <img src="./img/front.png" alt="Card Back">
                 </div>
-                <div class="back-face face">
-                    <img src="img/front.png" alt="Card Back">
-                </div>
-            </div>`
-            document.querySelector("section").innerHTML += template;
+            </li>`
+        document.querySelector("ul").innerHTML += template;
     }
 }
 
 criarCartas();
-
 function turnCard(elemento) {
-    let card = document.querySelectorAll(".card .back-face");
-    if (card !== null) {
-        card.classList.add("turn");
+    let selecionado = document.querySelectorAll(".card.selecionado");
+    
+
+
+    if (selecionado[0] === undefined) {
+        back = elemento.querySelector(".back-face");
+        front = elemento.querySelector(".front-face");
+        back.classList.add("back-face-flip");
+        front.classList.add("front-face-flip");
+        selecionado.classList.add("selecionado");
     }
-    elemento.classList.remove("turn");
 }
